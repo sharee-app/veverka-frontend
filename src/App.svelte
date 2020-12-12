f817e8a1a4af147f1b67115793ae02359751a01f
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-12 13:06:29 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let emoji;
+</script>
+
+<main>
+	<h1>Veverka Frontend is housed here {emoji}!</h1>
+	<p>Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
