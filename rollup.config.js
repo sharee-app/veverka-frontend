2d156e206a0b0acfeaea68ee6c94decdaae30ef7
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-12 14:00:55 +0100

    Tailwind stuff

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -40,7 +41,8 @@ export default {
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
-				dev: !production
+				dev: !production,
+				preprocess: sveltePreprocess({ postcss: true }),
 			}
 		}),
 		// we'll extract any component CSS out into
--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,12 @@
 </script>
 
 <main>
-	<h1>Veverka Frontend is housed here {emoji}!</h1>
+	<h1 class="font-bold">Veverka Frontend is housed here {emoji}!</h1>
 	<p>Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>  
