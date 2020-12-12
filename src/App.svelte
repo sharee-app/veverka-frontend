3e380f716f72e4344c5b1b8c62c824a093d918e7
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-12 15:59:06 +0100

    Update styling

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,23 @@
 	export let emoji;
 </script>
 
-<main>
-	<h1 class="font-bold">Veverka Frontend is housed here {emoji}!</h1>
-	<p>Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
+<main class="dark:bg-gray-800">
+	<h1 class="font-bold dark:text-white">Veverka Frontend is housed here {emoji}!</h1>
+	<p class="dark:text-white">Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
 </main>
 
-<style global>
+<style global lang="postcss">
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
+
+	body {
+		width: 100vw;
+		padding: 0 !important;
+		@apply dark:bg-gray-800;
+	}
+
+	main {
+    	padding: 50px;
+  	}
 </style>  
