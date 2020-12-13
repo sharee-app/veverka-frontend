32fb7165d67414153fc17233366168968fc8e0e9
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-13 13:18:29 +0100

    Title n' stuff

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 	export let emoji;
 </script>
 
-<main class="dark:bg-gray-800">
+<main>
 	<h1 class="font-bold dark:text-white">Veverka Frontend is housed here {emoji}!</h1>
 	<p class="dark:text-white">Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
 </main>
