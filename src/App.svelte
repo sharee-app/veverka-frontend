46a7c51444e215ba93c24a6250d986940858dde1
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-13 13:22:36 +0100

    Make heading bigger

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1 class="font-bold dark:text-white">Veverka Frontend is housed here {emoji}!</h1>
+	<h1 class="text-6xl font-bold dark:text-white">Veverka Frontend is housed here {emoji}!</h1>
 	<p class="dark:text-white">Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
 </main>
 
