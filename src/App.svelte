94304881905772ee44e963f23cf49a6a2d6b32c0
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-03 13:22:55 +0100

    Add frontend v1.1.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,125 @@
 <script>
-	export let emoji;
 </script>
 
 <main>
-	<h1 class="text-6xl font-bold dark:text-white">Veverka Frontend is housed here {emoji}!</h1>
-	<p class="dark:text-white">Check out our <a href="https://github.com/microsoftstc/veverka-frontend">repo</a> or else</p>
+	<div class="h-screen w-screen">
+		<div class="h-16 w-full flex">
+		   <div class="w-1/4 h-full">Sharee logo</div>
+		   <div class="w-2/4 h-4/5 mx-8 mt-1.5">
+			  <div class="flex h-full rounded-full border-grey-light border">
+				 <input class="h-full w-full mr-4 text-xl" type="text" placeholder="Search..." />
+			  </div>
+		   </div>
+		   <div class="w-1/4 h-full">Messenger Profile Points</div>
+		</div>
+		<div class="w-full flex">
+		   <div class="w-1/3 h-full border">
+			  <h2 class="text-3xl font-bold">🔥 Hot of the day</h2>
+			  <div class="bg-white border shadow-sm px-4 py-3 max-w-lg">
+				 <div class="flex items-center">
+					<img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+					<div class="ml-2">
+					   <div class="text-sm">
+						  <span class="font-semibold">Pepa z Depa</span>
+						  <span class="text-gray-500">@peepo</span>
+					   </div>
+					   <div class="text-gray-500 text-xs">to s/Vše</div>
+					   <div class="text-gray-500 text-xs flex">
+						  <span class="inline-block">3h • Edited </span>
+					   </div>
+					</div>
+				 </div>
+				 <p class="text-gray-800 text-sm mt-2 leading-normal md:leading-relaxed">
+					Gibbet lugsail aft chase guns list schooner barque galleon gun ho. Swab tender measured fer yer chains lateen sail poop deck mizzenmast tackle Yellow Jack prow barque. Wench chandler heave to line hogshead lookout Arr measured fer yer chains league belay.
+					Parley hogshead hang the jib yawl smartly league Barbary Coast schooner salmagundi nipperkin. Black jack flogging Nelsons folly cog fire ship to go on account hearties bowsprit careen squiffy. Jolly Roger lanyard chantey long clothes Shiver me timbers tackle mizzen chase mizzenmast parrel. 
+				 </p>
+				 <div class="text-gray-500 text-xs flex items-center mt-3 ml-3 justify-end">
+					<span class="ml-1 flex">
+					  <svg class="w-6 h-4 hover:text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>
+					  <svg class="w-6 h-4 hover:text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
+					  <svg class="w-6 h-4 hover:text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
+					  <span class="ml-3"> 47 </span>
+					   <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
+					   </svg>
+		
+					   <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+					   </svg>
+					</span>
+				 </div>
+			  </div>
+			  <div class="bg-white border shadow-sm px-4 py-3 max-w-lg">
+				 <div class="flex items-center">
+					<img class="h-12 w-12 rounded-full" src="https://thispersondoesnotexist.com/image" />
+					<div class="ml-2">
+					   <div class="text-sm">
+						  <span class="font-semibold">Adam von Prag</span>
+						  <span class="text-gray-500">@adameek</span>
+					   </div>
+					   <div class="text-gray-500 text-xs">to s/blondýnky</div>
+					   <div class="text-gray-500 text-xs flex">
+						  <span class="inline-block">5h</span>
+					   </div>
+					</div>
+				 </div>
+				 <p class="text-gray-800 text-sm mt-2 leading-normal md:leading-relaxed">
+					Muž přijde domů a vidí svou světlovlasou manželku, jak dává na zeď mokrý hadr. „Co to proboha děláš?" ptá se. „Pan doktor mi řekl, že mám dávat studené obklady na místo, kde jsem se odřela!"
+				 </p>
+			  <div class="text-gray-500 text-xs flex items-center mt-3 ml-3 justify-end">
+					<span class="ml-1 flex">
+					  <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>
+					  <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
+					  <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
+					  <span class="ml-3"> 69 </span>
+					   <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
+					   </svg>
+					   <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+					   </svg>
+					</span>
+				 </div>
+			  </div>
+			  <div class="bg-white border shadow-sm px-4 py-3 max-w-lg">
+				 <div class="flex items-center">
+					<img class="h-12 w-12 rounded-full" src="https://thispersondoesnotexist.com/image" />
+					<div class="ml-2">
+					   <div class="text-sm">
+						  <span class="font-semibold">Adam von Prag</span>
+						  <span class="text-gray-500">@adameek</span>
+					   </div>
+					   <div class="text-gray-500 text-xs">to s/blondýnky</div>
+					   <div class="text-gray-500 text-xs flex">
+						  <span class="inline-block">2d</span>
+					   </div>
+					</div>
+				 </div>
+				 <p class="text-gray-800 text-sm mt-2 leading-normal md:leading-relaxed">
+					Víte, proč blondýna zmrzla v letním kině ?
+					<br>
+					Chtěla vidět "Přes zimu ZAVŘENO."
+				 </p>
+				  <div class="text-gray-500 text-xs flex items-center mt-3 ml-3 justify-end">
+					<span class="ml-1 flex">
+					  <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>
+					  <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path></svg>
+					  <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
+					  <span class="ml-3"> 420 </span>
+					   <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
+					   </svg>
+					   <svg class="w-6 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+					   </svg>
+					</span>
+				 </div>
+			  </div>
+		   </div>
+		   <div class="w-1/3 h-full text-3xl font-bold">📃 Homepage</div>
+		   <div class="w-1/3 h-full text-3xl font-bold">⚡ Feed</div>
+		</div>	
+	</div>
 </main>
 
 <style global lang="postcss">
@@ -15,7 +130,6 @@
 	body {
 		width: 100vw;
 		padding: 0 !important;
-		@apply dark:bg-gray-800;
 	}
 
 	main {
