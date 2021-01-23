454006454880a8132cf6cbb5e2308b037dc348f8
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-23 14:05:22 +0100

    SignIn page - add Sharee logo

--- src/SignIn.svelte
+++ src/SignIn.svelte
@@ -4,9 +4,7 @@
 <main>
     <div class="text-center pt-16">
         <div class="flex items-center justify-center">
-        <svg fill="none" viewBox="0 0 24 24" class="w-12 h-12 text-sharee" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
-        </svg>
+            <img src="img/icon.svg" alt="" class="h-16 mb-8">
         </div>
         <h2 class="text-4xl tracking-tight">
         Sign in into your Sharee account
