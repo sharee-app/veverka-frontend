d18456a22372a3a400e2df08c015d0d01db5bbc7
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-09 18:15:44 +0100

    Responsive body

--- src/App.svelte
+++ src/App.svelte
@@ -32,7 +32,7 @@
 	<div class="h-screen w-screen dark:bg-gray-800 dark:text-white pr-12 pl-12">
 	  <Menu theme={themeMode} />
 	  <div class="flex">
-		<div class="w-1/5 h-full">
+		<div class="w-1/5 h-full hidden lg:block">
 		  <h2 class="text-3xl font-bold flex">
 			<svg class="w-9 h-9 mr-2 head-icon text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"></path>
@@ -87,7 +87,7 @@
 				score={42}
 			/>
 		</div>
-		<div class="w-1/5 h-full">
+		<div class="w-1/5 h-full hidden lg:block">
 			<h2 class="text-3xl font-bold flex">
 				<svg class="w-9 h-9 mr-2 head-icon text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
