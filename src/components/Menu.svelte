2ba2003f3b86b614edf202e73867668348b87a2c
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-23 18:02:19 +0100

    Logo responsivity

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -3,9 +3,14 @@
 </script>
 
 <div class="h-16 w-full flex mb-12 pt-5 justify-between">
-    <div class="w-60 h-full mt-4 justify-start"><a href="/"><img src={theme === "dark" ? 'img/logo.svg' : 'img/light.svg' } class="max-h-full" alt="Sharee logo"></a></div>
+    <div class="w-60 h-full mt-4 justify-start">
+      <a href="/">
+        <img src={theme === "dark" ? 'img/logo.svg' : 'img/light.svg' } class="max-h-full hidden md:block" alt="Sharee logo">
+        <img src="img/icon.svg" class="max-h-full block md:hidden" alt="Sharee logo">
+      </a>
+    </div>
     <div class="w-2/4 h-4/5 mt-1.5">
-      <div class="relative">
+      <div class="relative hidden lg:block">
         <div class="absolute top-4 left-2">
           <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
